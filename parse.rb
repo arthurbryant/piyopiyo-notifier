db0dd272a3c6db0ecd8b1136b7d50117fe41899f
arthurbryant <cst.feng@gmail.com>
2023-03-12 14:25:48 +0900

    feat: rubocop -a

--- parse.rb
+++ parse.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'open-uri'
 
@@ -5,11 +7,11 @@ require 'open-uri'
 def get_available_days
   doc = Nokogiri::HTML(open('https://www.nerima-piyopiyo.com/piyopiyo/useddate/select/01.html'))
   doc.css('td').each_slice(4) do |slice|
-    dt = slice.shift.text.gsub(/\s+/, "")
-    marks = slice.map { |td| td.text.gsub(/\s+/, "") }
+    dt = slice.shift.text.gsub(/\s+/, '')
+    marks = slice.map { |td| td.text.gsub(/\s+/, '') }
     marks.each_with_index do |m, index|
-      if m == '○' or m == '△'
-        puts dt + " " + (index + 1).to_s + "枠は" + m + " です"
+      if (m == '○') || (m == '△')
+        puts dt + ' ' + (index + 1).to_s + '枠は' + m + ' です'
       end
     end
   end
