7acff709f429a802865a7728b6d3e929f5ac4a58
arthurbryant <cst.feng@gmail.com>
2023-03-31 22:44:20 +0900

    fix: next month

--- jp_holiday_generator.rb
+++ jp_holiday_generator.rb
@@ -1,11 +1,12 @@
 require 'holiday_jp'
 require 'active_support/time'
 require 'awesome_print'
+require 'date'
 
 class JpHolidayGenerator
   def self.generate
     # show the holidays in one month
-    HolidayJp.between(Time.now, 1.month.from_now).map do |holiday|
+    HolidayJp.between(Time.now, 1.month.from_now(Date.today)).map do |holiday|
       holiday.date.strftime('%Y年%m月%d日')
     end
   end
